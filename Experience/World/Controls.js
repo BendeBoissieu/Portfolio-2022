e5f8c44654a6f9daebb1f6dfcda5109d022d28d2
Benjamin de Boissieu <bendeb@hotmail.com>
2022-11-04 14:01:00 +0100

    splitting views, camera moving and centered

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -23,6 +23,16 @@ export default class Controls {
         // To have the next point of the line
         this.lookAtPosition = new THREE.Vector3(0,0,0);
 
+        // Buidling a vector to rotate the camera to the center
+        // It is builde from the position vector and the lookAtPosition vector (which is the next point)
+        this.directionalVector = new THREE.Vector3(0,0,0);
+
+        // The staticvector will give the direction of the camera to look at the center and not outside
+        this.staticVector = new THREE.Vector3(0,1,0);
+
+        // The  crossvector is the result betweeen directional and static vectors
+        this.crossVector = new THREE.Vector3(0,0,0);
+
         this.setPath();
         this.onWheel();
     }
@@ -30,11 +40,10 @@ export default class Controls {
     setPath() {
       //Create a closed wavey loop
       this.curve = new THREE.CatmullRomCurve3( [
-        new THREE.Vector3( -10, 0, 10 ),
-        new THREE.Vector3( -5, 5, 5 ),
-        new THREE.Vector3( 0, 0, 0 ),
-        new THREE.Vector3( 5, -5, 5 ),
-        new THREE.Vector3( 10, 0, 10 )
+        new THREE.Vector3( -5, 0, 0 ),
+        new THREE.Vector3( 0, 0, -5 ),
+        new THREE.Vector3( 5, 0, 0 ),
+        new THREE.Vector3( 0, 0, 5 )
       ], true );
 
       const points = this.curve.getPoints( 50 );
@@ -69,26 +78,55 @@ export default class Controls {
         this.lerp.current,
         this.lerp.target,
         this.lerp.ease
-      );
-
-      // to move automatically
-      if (this.back) {
-        this.lerp.target += 0.001;
-      } else {
-        this.lerp.target -= 0.001;
-      }
+        );
 
       this.lerp.target = GSAP.utils.clamp(0, 1, this.lerp.target);
       this.lerp.current = GSAP.utils.clamp(0, 1, this.lerp.current);
-
-      this.curve.getPointAt(this.lerp.current % 1, this.position);
-
-      this.curve.getPointAt(this.lerp.current + 0.00001 % 1, this.lookAtPosition);
-
+      // Method to follow the path center to the center point
+      // copy the postion in the interpolate point
+      this.curve.getPointAt(this.lerp.current %1, this.position);
+      // put the camera on this position
       this.camera.orthographicCamera.position.copy(this.position);
 
-      // To make the camera follow the curve with the angle
-      // If we desactivate it the camera follow the curve without rotate
-      this.camera.orthographicCamera.lookAt(this.lookAtPosition);
+      // Substracts the vectors
+      this.directionalVector.subVectors(
+        this.curve.getPointAt((this.lerp.current %1) + 0.000001),
+        this.position
+      );
+      // Normalize the vector
+      this.directionalVector.normalize();
+
+      // buidl the cross vector
+      this.crossVector.crossVectors(
+        this.directionalVector,
+        this.staticVector
+      );
+
+      // Multiply scalar vector
+      this.crossVector.multiplyScalar(10000);
+
+      // them move and orientate the camera
+      this.camera.orthographicCamera.lookAt(0,0,0);
+
+      // Method to follow a path smoothly
+      // // to move automatically
+      // if (this.back) {
+      //   this.lerp.target += 0.001;
+      // } else {
+      //   this.lerp.target -= 0.001;
+      // }
+
+      // this.lerp.target = GSAP.utils.clamp(0, 1, this.lerp.target);
+      // this.lerp.current = GSAP.utils.clamp(0, 1, this.lerp.current);
+
+      // this.curve.getPointAt(this.lerp.current % 1, this.position);
+
+      // this.curve.getPointAt(this.lerp.current + 0.00001 % 1, this.lookAtPosition);
+
+      // this.camera.orthographicCamera.position.copy(this.position);
+
+      // // To make the camera follow the curve with the angle
+      // // If we desactivate it the camera follow the curve without rotate
+      // this.camera.orthographicCamera.lookAt(this.lookAtPosition);
     }
 }
